8bcc2ff0c31b5f8126ab68914c9bf1bbe5dcc417
Yogesh Lamba <yogeshlamba021@gmail.com>
2023-11-05 16:03:29 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Yogesh Lamba on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
